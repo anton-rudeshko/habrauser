c698c65c6c368a8798b89f211785d18aafd21bf7
Anton Rudeshko <anton@rudeshko.com>
2012-11-01 23:31:36 +0400

    Making defaults

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,20 @@
-(function () {
-  function loadCheckbox(options, id) {
-    options[id] = localStorage[id] || 'true';
+(function (localStorage) {
+
+  function loadStorage(key, defaultValue) {
+    localStorage[key] = localStorage[key] || defaultValue;
   }
 
+  loadStorage('user-popup', 'true');
+  loadStorage('highlight-author', 'true');
+  loadStorage('highlight-comments', 'false');
+
   function loadOptions() {
-    var options = {};
-    loadCheckbox(options, 'user-popup');
-    loadCheckbox(options, 'highlight-author');
-    loadCheckbox(options, 'highlight-comments');
+    var key, options = {};
+    for (key in localStorage) {
+      if (localStorage.hasOwnProperty(key)) {
+        options[key] = localStorage[key];
+      }
+    }
     return options;
   }
 
@@ -15,4 +22,5 @@
     var response = request.method == 'getOptions' ? {options: loadOptions()} : {};
     sendResponse(response);
   });
-}());
+
+}(localStorage));
