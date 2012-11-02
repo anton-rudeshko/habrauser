3723e0015c539ba4c0688f2f7325e310085cd612
Anton Rudeshko <anton@rudeshko.com>
2012-11-03 03:39:35 +0400

    Hooked up onInstalled handler

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,12 +1,37 @@
 (function (chrome, document) {
-  var request = new XMLHttpRequest();
-  request.open('GET', chrome.extension.getURL('js/main.js'), false);
-  request.send(null);
+  function getUrl(url) {
+    return chrome.extension.getURL(url);
+  }
 
-  chrome.storage.sync.get(null, function (options) {
+  function getResource(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', getUrl(url), false);
+    request.send(null);
+    return request.responseText;
+  }
+
+  function showUpdate(version) {
+    var html = getResource('html/update.html');
+    html = html.replace('{{version}}', version);
+    html = html.replace('{{options-url}}', getUrl('html/options.html'));
+
+    var element = document.createElement('div');
+    element.innerHTML = html;
+
+    document.body.appendChild(element);
+  }
+
+  var sync = chrome.storage.sync;
+
+  sync.get(null, function (options) {
+    if (options['new-version']) {
+      showUpdate(options['new-version']);
+      sync.set({'new-version': ''});
+    }
+
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.text = 'window.Habrauser = ' + JSON.stringify(options || {}) + ';' + request.responseText;
+    script.text = 'window.Habrauser=' + JSON.stringify(options || {}) + ';' + getResource('js/main.js');
     document.body.appendChild(script);
   });
 
