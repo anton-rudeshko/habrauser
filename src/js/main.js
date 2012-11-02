44b75c3c27b444f5db6ba40d20c3453d905c0d32
Anton Rudeshko <anton@rudeshko.com>
2012-11-03 03:44:51 +0400

    Releasing 0.8.6

--- src/js/main.js
+++ src/js/main.js
@@ -88,73 +88,8 @@
     $('.content_left').find('a[href="' + authorLink + '"]').addClass('habrauser__author-comment').attr('title', 'Автор');
   }
 
-  function colorTopComments() {
-    if (optionDisabled('highlight-comments')) {
-      return;
-    }
-
-    var positiveScore = function positiveScore(item) {
-      return item.score > 0
-    };
-
-    var commentsDescending = function commentsDescending(comment1, comment2) {
-      return comment2.score - comment1.score;
-    };
-
-    function CommentItem(element) {
-      var $el = $(element),
-        info = $el.children('.info'),
-        score = +info.find('.score').text();
-
-      this.comment = $el;
-      this.info = info;
-      this.score = isNaN(score) ? 0 : score;
-      this.message = $el.children('.message');
-    }
-
-    CommentItem.prototype = {
-      MAIN_COLOR: '210, 248, 205',
-
-      applyOpacity: function (opacity) {
-        var color = 'rgba(' + this.MAIN_COLOR + ', ' + opacity + ')';
-
-        this.message.css('background-color', color);
-        this.info.css('background-color', color);
-      }
-    };
-
-    var toCommentItem = function toCommentItem() {
-      return new CommentItem(this);
-    };
-
-    var comments = $('.comment_item, .answer').map(toCommentItem).get();
-    comments = $.grep(comments, positiveScore);
-    comments.sort(commentsDescending);
-
-    var highlightFactor = .20;
-    var count = comments.length*highlightFactor >> 0;
-    count = Math.max(count, 2);
-    count = Math.min(count, 15);
-    comments = comments.slice(0, count);
-
-    // todo: filter low score
-
-    function applyColorToComments(comments) {
-      var maxOpacity = 1.0,
-        length = comments.length,
-        step = maxOpacity/(length + 1);
-
-      for (var i = 0; i < length; ++i) {
-        comments[i].applyOpacity(maxOpacity - i*step);
-      }
-    }
-
-    applyColorToComments(comments);
-  }
-
   function onLoad() {
     colorAuthor();
-    colorTopComments();
     hookPopup();
   }
 
